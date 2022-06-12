7496e2e0a09102c0ef87b7f3c51f5041207573ea
dreammnck <dreamnitchakran2543@gmail.com>
2022-06-12 22:31:45 +0700

    Fix misunderstanding the question

--- src/sorting.ts
+++ src/sorting.ts
@@ -1,3 +1,22 @@
 export const merge = (collection_1: Array<Number>, collection_2: Array<number>) => {
-    return collection_1.concat(collection_2);
+    let returnCollection = [];
+    let idx_1 = 0;
+    let idx_2 = 0;
+    while(idx_1 < collection_1.length || idx_2 < collection_2.length) {
+        if(collection_1.length === idx_1) {
+            return returnCollection.concat(collection_2.splice(idx_2, collection_2.length))
+        }
+        if(collection_2.length === idx_2) {
+            return returnCollection.concat(collection_1.splice(idx_1, collection_1.length))
+        }
+        if(collection_1[idx_1] <= collection_2[idx_2]) {
+            returnCollection.push(collection_1[idx_1]);
+            idx_1 += 1;
+        }
+        else {
+            returnCollection.push(collection_2[idx_2])
+            idx_2 += 1;
+        }
+    }
+    return returnCollection;
 }
--- src/test/sorting.spec.ts
+++ src/test/sorting.spec.ts
@@ -2,11 +2,19 @@ import {merge} from '../sorting'
 
 describe("merging test", () => {
     it("Result", () => {
-        expect(merge([1,2],[1,2])).toStrictEqual([1,2,1,2])
+        expect(merge([1,2],[1,2])).toStrictEqual([1,1,2,2])
     });
 
     it("Result", () => {
         expect(merge([1,2,3],[4,5,6])).toStrictEqual([1,2,3,4,5,6])
+    });
+
+    it("Result", () => {
+        expect(merge([1,2,3,4],[3,7])).toStrictEqual([1,2,3,3,4,7])
+    });
+
+    it("Result", () => {
+        expect(merge([4,5],[1,2,3])).toStrictEqual([1,2,3,4,5])
     });
 
 });
